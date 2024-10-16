7f4465cb99ce860ddfb842ce7c678f9294a46c4f
18degrees <mikhaili.dev@gmail.com>
2024-10-16 08:35:39 +0300

    SEO: set redirect from www

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    redirects: async () => {
+        return [
+            {
+                source: '/:path*',
+                has: [{ type: 'header', key: 'host', value: 'www.exhale.pics' }],
+                destination: 'https://exhale.pics/:path*',
+                permanent: true
+            }
+        ]
+    }
+}
 
 export default nextConfig
