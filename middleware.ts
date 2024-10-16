884492f4f842b0a412f176e8104c849cc27fbb11
18degrees <mikhaili.dev@gmail.com>
2024-10-16 08:34:35 +0300

    SEO: set redirect to https

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,41 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+
 export {default} from 'next-auth/middleware'
 
 export const config = {
-    matcher: ['/admin']
+    matcher: '/:path?'
+}
+
+const protectedPaths = ['/admin']
+
+type environment = "production" | "development"
+
+export async function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname
+
+    const isPathProtected = protectedPaths.includes(path)
+
+    if (isPathProtected) {
+        const session = await getToken({req: request, secret: process.env.NEXTAUTH_SECRET})
+    
+        if (!session) {
+            const origin = request.nextUrl.origin
+
+            return NextResponse.redirect(`${origin}/api/auth/signin?callbackUrl=${path}`, 308)
+        }
+
+    }
+    const currentEnv = process.env.NODE_ENV as environment
+
+    const isHttps = request.nextUrl.protocol === "https:"
+    const isLocalhost = request.nextUrl.origin.includes("localhost")
+
+    if (currentEnv === 'production' && !isLocalhost && !isHttps) {
+        const newUrl = new URL(request.nextUrl.href)
+
+        newUrl.protocol = "https:"
+
+        return NextResponse.redirect(newUrl.href, 301)
+    }
 }
