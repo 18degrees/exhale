b52b8b5a4a84461b0e32dd8bf46836d638df7469
18degrees <mikhaili.dev@gmail.com>
2024-11-04 10:37:52 +0300

    style: change favicon

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,22 +1,18 @@
 import Link from 'next/link'
 import style from './header.module.css'
-import { barlow } from 'app/fonts'
 import Image from 'next/image'
 
 export default function Header() {
     return (
         <header className={style.header}>
             <div>
-                <div className={style['logo-container']}>
-                    <Image
-                        src='/favicon-rounded-45x45.png'
-                        alt='logo'
-                        width={45}
-                        height={45}
-                        priority
-                    />
-                    <span className={barlow.className}>exhale</span>
-                </div>
+                <Image
+                    src='/favicon-48x48.png'
+                    alt='logo'
+                    width={48}
+                    height={48}
+                    priority
+                />
                 <nav>
                     <Link href='/'>gallery</Link>
                     <Link href='/about'>about</Link>
--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
 			<Providers>
 				<head>
 					<link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+					<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
 					<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 					<link rel="shortcut icon" href="/favicon.ico" />
 					<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
